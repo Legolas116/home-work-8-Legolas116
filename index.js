be69b5c981f023ac690f2c86893b5e596e14bf15
Erik Kotsiuba <ErikKotsiuba@udelphi.com>
2017-02-19 03:30:26 +0200

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+function setText(selector, text) {
+	/*
+		`selector` is string with CSS-like selector
+		`text` - is just string with text we want to change
+	*/
+
+	// Write your code here
+}
+
+
+function setTextAll(selector, text) {
+	// Write your code here
+}
+
+
+setText("p", "wasn't that hard, yeah ?");
+setTextAll("div .block a", "or was it ?");
